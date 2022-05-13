39e8bbcaf25a9b25549176ad60e804548e2aafd8
Karen Chibukhchyan <karenchikarenchi@gmail.com>
2022-05-13 21:26:38 +0400

    Added BaseManager

--- UniversityConsoleApp/BL/StudentManager.cs
+++ UniversityConsoleApp/BL/StudentManager.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using UniversityConsoleApp.Models;
+
 namespace UniversityConsoleApp.BL
 {
     public class StudentManager : BaseManager
     {
         const short maxAge = 139;
         public override Person Create(string firstName, string lastName, int age) => new Student(firstName, lastName, age);
+
         public override Person[] Create(int count, int minAge)
         {
             Student[] students = new Student[count];
@@ -14,6 +16,7 @@ namespace UniversityConsoleApp.BL
             {
                 students[i] = new Student($"name{i}", $"surName{i}", rnd.Next(minAge, maxAge));
             }
+
             return students;
         }
 
@@ -23,11 +26,13 @@ namespace UniversityConsoleApp.BL
             Console.WriteLine($"id:{person.ID} name:{person.FirstName} lastName:{person.LastName} age:{person.Age}");
             if (person is Student)
             {
-                if (((Student)person).Teacher == null)
+                if (((Student) person).Teacher == null)
                     Console.WriteLine("This student does not have a teacher!");
                 else
-                    Console.WriteLine($"Teacher of this student: id:{((Student)person).Teacher.ID} name:{((Student)person).Teacher.FirstName} lastName:{((Student)person).Teacher.LastName} age:{((Student)person).Teacher.Age}");
+                    Console.WriteLine(
+                        $"Teacher of this student: id:{((Student) person).Teacher.ID} name:{((Student) person).Teacher.FirstName} lastName:{((Student) person).Teacher.LastName} age:{((Student) person).Teacher.Age}");
             }
+
             Console.WriteLine();
         }
 
